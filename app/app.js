d205dc38110fccf7b62c8b090059be059725de9b
tara-k <37451334+tara-k@users.noreply.github.com>
2020-04-04 21:41:47 -0700

    adding in some test data

--- app/app.js
+++ app/app.js
@@ -15,8 +15,30 @@ $(document).ready(function() {
 
   const db = firebase.database()
   const roomname = "name"
+  const test_room = {
+    "name": "kristen_and_tara",
+    "id": 1,
+    "current_question": 4,
+    "time_remaining": 34,
+    "state": "playing",
+    "players": [
+      {"name": "kristen",
+       "id": 2,
+       "score": 10,
+      }, 
+      {"name": "tara",
+       "id": 3,
+       "score": 11,
+      },],
+  }
+  //Name from input in html
+  //ID - generate automatically
+  //Current question - init to none, grab questions from API based on game state and time remaining
+  //Time remaining - function
+  //State - function
+  //Players - name: input, id: generate, score: generate 
 
-  db.ref().child('rooms').set([roomname, roomname])
+  db.ref().child('rooms').set([test_room])
 
   db.ref().on("value", function(snapshot) {
     const data = snapshot.val()["test-data"]
