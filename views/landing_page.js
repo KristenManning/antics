e18a45d5c317ffef3e6942d5eec6afa59d2f2262
tara-k <37451334+tara-k@users.noreply.github.com>
2020-04-05 15:59:55 -0700

    basically nothing new

---
+++ views/landing_page.js
@@ -0,0 +1,19 @@
+function show_create_room_details() {
+                var x = document.getElementById("create_room_button");
+                if(x.style.display === 'none') {
+                    x.style.display = 'block';
+                }
+                else {
+                    x.style.display = 'none';
+                }
+            }
+
+function show_join_room_details() {
+    var y document.getElementById("join_existing_room");
+    if(y.style.display === 'none') {
+        y.style.display = 'block';
+    }
+    else {
+        y.style.display = 'none';
+    }
+}
