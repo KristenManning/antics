24611ad96c69063641fe3bb30374c152b6099d9e
Kristen Manning <kristenmanning16@gmail.com>
2020-04-04 21:57:59 -0700

    create functions for initializing room, player

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // Your web app's Firebase configuration
-  var firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyAEOczSaNFhg7_eZNN7G_rCUWRkVLaokPc",
     authDomain: "antics-538ac.firebaseapp.com",
     databaseURL: "https://antics-538ac.firebaseio.com",
@@ -12,25 +12,42 @@ $(document).ready(function() {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  const create_player = (id, name) => {
+    return {
+      "id": id, 
+      "name": name, 
+      "score": 0, 
+    }
+  }
+  const create_room = (id, name, room_leader) => { 
+    return {
+      "id": id, 
+      "name": name,
+      "current_question": null,
+      "time_remaining": null, 
+      "state": "waiting_to_begin",
+      "players": [room_leader],
+      "room_leader": room_leader, 
+    }
+  };
 
   const db = firebase.database()
-  const roomname = "name"
-  const test_room = {
-    "name": "kristen_and_tara",
-    "id": 1,
-    "current_question": 4,
-    "time_remaining": 34,
-    "state": "playing",
-    "players": [
-      {"name": "kristen",
-       "id": 2,
-       "score": 10,
-      }, 
-      {"name": "tara",
-       "id": 3,
-       "score": 11,
-      },],
-  }
+  // const test_room = {
+  //   "name": "kristen_and_tara",
+  //   "id": 1,
+  //   "current_question": 4,
+  //   "time_remaining": 34,
+  //   "state": "playing",
+  //   "players": [
+  //     {"name": "kristen",
+  //      "id": 2,
+  //      "score": 10,
+  //     }, 
+  //     {"name": "tara",
+  //      "id": 3,
+  //      "score": 11,
+  //     },],
+  // }
   //Name from input in html
   //ID - generate automatically
   //Current question - init to none, grab questions from API based on game state and time remaining
@@ -38,7 +55,7 @@ $(document).ready(function() {
   //State - function
   //Players - name: input, id: generate, score: generate 
 
-  db.ref().child('rooms').set([test_room])
+  db.ref().child('rooms').set([create_room(1, "Test Room", create_player(1, "Test Player"))])
 
   db.ref().on("value", function(snapshot) {
     const data = snapshot.val()["test-data"]
